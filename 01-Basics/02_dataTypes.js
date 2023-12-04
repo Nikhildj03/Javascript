7515f2d1dc0ed8a33fa6425f067524bca5d002e8
Nikhil Kakade <nikhilkakadedj@gmail.com>
2023-12-04 12:10:01 +0530

    learn the Data type

---
+++ 01-Basics/02_dataTypes.js
@@ -0,0 +1,30 @@
+"use strict";   // treat all JS code as newer version
+
+alert( 3 + 3) // we are using the nodejs , not browser
+
+console.log( 3
+    + 
+    3)  // code readability should be high
+
+    console.log("Nikhil");
+
+    let name = "Nikhil"
+    let age = 18
+    let isLoggedIn = true
+    let state = null
+
+
+    // number => 2 to power 53 
+    // bigint  use in big amount of number like stock market
+    // String => ""
+    // boolean => true/false
+    // null => standalone value
+    // undefined =>
+    // symbol => unique
+
+
+
+    // object
+
+    console.log(typeof undefined); // undefined
+    console.log(typeof null); //Object

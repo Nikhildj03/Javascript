425598191371c0b1da073583a1b02a839aa06d4a
Nikhil Kakade <nikhilkakadedj@gmail.com>
2023-12-04 18:52:17 +0530

    comparision

---
+++ 01-Basics/04_comparision.js
@@ -0,0 +1,25 @@
+// console.log(2 > 1);
+// console.log(2 < 1);
+// console.log(2 >= 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+// console.log("nikhil" >= "Nikhil"); //true
+// console.log("N" >= "1");  //true
+
+// console.log(null > 0);   // f
+// console.log(null == 0);   // f
+// console.log(null >= 0);   // t 
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+//  we have to not used sch type of coding where uor readablity is compromise
+
+// Strictly === 
+ 
+console.log("2" === 2 );

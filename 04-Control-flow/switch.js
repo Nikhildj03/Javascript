5714f81ba2227ddd585abf08b856724172af958e
Nikhil Kakade <nikhilkakadedj@gmail.com>
2023-12-06 16:20:30 +0530

    Control-flow-files

---
+++ 04-Control-flow/switch.js
@@ -0,0 +1,54 @@
+// switch (key) {
+//     case value:
+
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = "march"
+
+switch (month) {
+    case "jan":
+        console.log("January");
+        break;
+    case "feb":
+        console.log("Feb");
+        break;
+    case "march":
+        console.log("MARCH");
+        break;
+    case "april":
+        console.log("APRIL");
+        break;
+    case "may":
+        console.log("January");
+        break;
+    case "june":
+        console.log("june");
+        break;
+    case "july":
+        console.log("july");
+        break;
+    case "August":
+        console.log("August");
+        break;
+    case "September":
+        console.log("September");
+        break;
+    case "Octomber":
+        console.log("Octomber");
+        break;
+    case "November":
+        console.log("November");
+        break;
+    case "december":
+        console.log("december");
+        break;
+
+        default:
+            console.log("Invalid input");
+            break;
+}
+

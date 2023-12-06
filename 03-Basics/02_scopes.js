f0a335eae0d2cd5ad290bf7ec876d5164e8b65a3
Nikhil Kakade <nikhilkakadedj@gmail.com>
2023-12-06 11:50:55 +0530

    Scopes

---
+++ 03-Basics/02_scopes.js
@@ -0,0 +1,60 @@
+var c = 300 
+  
+// Global scope
+
+
+let a = 300 
+if (true) {   // Block scope
+    let a = 10
+    let b = 20
+    console.log("Inner: " , a);
+}
+
+
+// What ever is present in Global block scope can accept it but what ever is present in the block scope it cannot access by  the global scope.
+
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+
+
+function one(){
+    const username = "nikhil"
+
+    function two(){
+        const website= "youtube"
+        console.log(username)
+     }
+//    console.log(website)
+     two()
+}
+
+// one()
+
+if(true){
+    const username = "nikhil"
+    if (username === "nikhil"){
+        const website = "youtube"
+        console.log(username + website);
+     }
+    //  console.log(website); 
+}
+
+// console.log(username);
+
+
+// +++++++++++++++++++Intereseting+++++++++++++++++++++++
+
+
+console.log(addone(5));
+
+function addone(num) {
+    return num + 1
+}
+
+addTwo(5)    //Error
+const addTwo = function(num){
+    return num + 2
+}

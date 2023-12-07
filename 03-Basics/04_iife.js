6193c056b56dc82a76e53875c257ab69e6d99d50
Nikhil Kakade <nikhilkakadedj@gmail.com>
2023-12-07 14:25:17 +0530

    Update 04_iife.js

--- 03-Basics/04_iife.js
+++ 03-Basics/04_iife.js
@@ -1,12 +1,12 @@
 // Immediately Invoked Function Expressions(IIFE)
 
-// we use iife to remove the global scope pollution problem and to remove that we use iife.
+// we use IIfe to remove the global scope pollution problem and to remove that we use iife.
 
 
 (function chai (){
     // named IIFE
     console.log(`DB CONNECTED`);
-})(); 
+})();     
 
 ((name) => {
  console.log(`DB Connected ${name}`);

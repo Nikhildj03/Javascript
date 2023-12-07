0821b1de2b4e99c19ebc8f95e43a07f93efa9bf0
Nikhil Kakade <nikhilkakadedj@gmail.com>
2023-12-07 14:24:29 +0530

    While and Do-While Loop

---
+++ 05-Iterations/two.js
@@ -0,0 +1,23 @@
+// node 05-Iterations/two.js
+
+
+// let index = 0
+// while (index <=10) {
+//     console.log(`Value of index is ${index}`);
+//     index = index + 2 
+// }
+
+// let myArry = ["Nikhil", "Kailas", "Kakade"]
+// let arr = 0
+// while (arr < myArry.length) {
+//    console.log(`Value is ${myArry[arr]}`);
+//    arr++
+// }
+
+
+let score = 11
+
+do { 
+    console.log(`Score is ${score}`);
+    score++
+} while (score <= 10);
